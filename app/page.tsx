7956288c97697f455774043334bc14762561b9d9
Eric Chu <eric@ericchu.net>
2025-04-23 01:49:41 -0700

    Add beggar links

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
         <h1 className="text-8xl">Monster</h1>
         <small>By Eric Chu</small>
       </div>
-      <a href="https://github.com/ericchu94/monster" target="_blank" rel="noopener noreferrer"><Image src="https://img.shields.io/badge/GitHub-%23121011.svg" alt="Github" width={47} height={20} /></a>
+      <a href="https://github.com/ericchu94/monster" target="_blank" rel="noopener noreferrer"><Image src="https://img.shields.io/badge/GitHub-%23121011.svg" alt="Github" width={94} height={40} /></a>
+      <Image src="/bmc_qr.png" alt="Buy Me A Coffee QR Code" height={150} width={150} className="m-4" />
+      <a href="https://www.buymeacoffee.com/ericchu" target="_blank"><Image src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height={60} width={217} /></a>
     </div>
   );
 }
