4133d16313d26ce9bb4f6ba79ed338f17102f9d1
Eric Chu <eric@ericchu.net>
2025-05-05 09:13:09 -0700

    Slightly optimize get all matchups

--- services/algs/utils.ts
+++ services/algs/utils.ts
@@ -36,9 +36,9 @@ export function getAllMatchUps<T>(items: T[]): T[][] {
     const n = items.length;
 
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
+        for (let j = i + 1; j < n; j++) {
             for (let k = 0; k < n; k++) {
-                for (let l = 0; l < n; l++) {
+                for (let l = k + 1; l < n; l++) {
                     if (i !== j && i !== k && i !== l && j !== k && j !== l && k !== l) {
                         matchUps.push([items[i], items[j], items[k], items[l]]);
                     }
