6d3cbb7810280428f5b1a41fe8254b8386c29cfd
Eric Chu <eric@ericchu.net>
2025-05-09 21:29:07 -0700

    Try viewport-fit=cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui, interactive-widget=resizes-content" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
